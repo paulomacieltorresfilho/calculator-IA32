70fb7214a0080a7098f821072263747a6de9c93e
Paulo Filho <paulomacieltorresfilho@gmail.com>
2026-07-07 23:20:16 -0300

    wip

--- CALCULADORA.asm
+++ CALCULADORA.asm
@@ -11,10 +11,13 @@ HOLA_SUFFIX_LEN EQU $-HOLA_SUFFIX
 NWLN            db 0Dh, 0Ah
 NWLN_LEN        EQU $-NWLN
 
+PRECISION       db 'Vai trabalhar com 16 ou 32 bits (digite 0 para 16, e 1 para 32): '
+PRECISION_LEN   EQU $-PRECISION
+
 username_len    dd 16
     
 section .bss
-digit_buffer resb 1
+precision resb 1
 username resb 16
 
 section .text
@@ -22,42 +25,63 @@ global _start
 ; extern soma
 
 _start:
+    enter 4,0
     push WELCOME
     push WELCOME_LEN
     call print
-    add esp, 8
-
-    push username
-    push DWORD [username_len]
-    call read_string
-    add esp, 8
-
-    sub al, 1
-    mov byte [username_len], al
 
     call print_hola
+
+    call get_precision
+
+    push precision
+    call printnum
 
 exit:
     mov eax, 1
     mov ebx, 0
     int 80h
 
+get_precision:
+    enter 0,0
+
+    push PRECISION
+    push PRECISION_LEN
+    call print
+
+    push precision
+    push 1
+    call read_string
+
+    mov eax, [precision]
+    sub eax, 0x30
+
+    mov [precision], eax ;ARRUMAR
+
+    leave
+    ret
+
 print_hola:
     enter 0,0
+
+    push username
+    push DWORD [username_len]
+    call read_string
+
+    sub al, 1
+    mov byte [username_len], al
+
     push HOLA_PREFIX
     push HOLA_PREFIX_LEN
     call print
-    add esp, 8
 
     push username
     push DWORD [username_len]
     call print
-    add esp, 8
 
     push HOLA_SUFFIX
     push HOLA_SUFFIX_LEN
     call println
-    add esp, 8
 
     leave
     ret
@@ -67,19 +91,17 @@ print_hola:
 printnum:
     enter 0,0
 
-    mov eax, [EBP+8]
-    add eax, 48         ; Convert to ASCII
-    
-    mov byte [digit_buffer], al  ; Store character in buffer
-    
+    mov ecx, [EBP+8]
+    mov ecx, [ecx]
+    add ecx, 0x30
+        
     mov eax, 4          ; write syscall
     mov ebx, 1          ; stdout
-    mov ecx, digit_buffer  ; pointer to buffer
     mov edx, 1          ; 1 byte
     int 80h
 
     leave
-    ret
+    ret 4
 
 ; print (*str, size)
 print:
@@ -92,7 +114,7 @@ print:
     int 80h
 
     leave
-    ret
+    ret 8 
 
 ; println (*str, size)
 println:
@@ -111,7 +133,7 @@ println:
     int 80h
 
     leave
-    ret
+    ret 8
 
 ;  read_string (*str, size)
 read_string:
@@ -124,5 +146,5 @@ read_string:
     int 80h ; eax -> tamanho da string inputada
 
     leave
-    ret
+    ret 8
 
