a01a9037a3a49aaf7d75a140ec5154190cb6252c
Paulo Filho <paulomacieltorresfilho@gmail.com>
2026-07-08 20:05:48 -0300

    corrige problemas printnum e adiciona prints do menu

--- CALCULADORA.asm
+++ CALCULADORA.asm
@@ -1,41 +1,48 @@
 section .data
 
-WELCOME         db  'Bem-vindo. Digite seu nome: ', 0
-WELCOME_LEN     EQU $-WELCOME
+    WELCOME         db  'Bem-vindo. Digite seu nome: ',
+    WELCOME_LEN     EQU $-WELCOME
 
-HOLA_PREFIX     db 'Hola, '
-HOLA_PREFIX_LEN EQU $-HOLA_PREFIX
-HOLA_SUFFIX     db ', bem-vindo ao programa de CALC IA-32'
-HOLA_SUFFIX_LEN EQU $-HOLA_SUFFIX
+    HOLA_PREFIX     db 'Hola, '
+    HOLA_PREFIX_LEN EQU $-HOLA_PREFIX
+    HOLA_SUFFIX     db ', bem-vindo ao programa de CALC IA-32'
+    HOLA_SUFFIX_LEN EQU $-HOLA_SUFFIX
 
-NWLN            db 0Dh, 0Ah
-NWLN_LEN        EQU $-NWLN
+    NWLN            db 0Dh, 0Ah
+    NWLN_LEN        EQU $-NWLN
 
-PRECISION       db 'Vai trabalhar com 16 ou 32 bits (digite 0 para 16, e 1 para 32): '
-PRECISION_LEN   EQU $-PRECISION
+    PRECISION       db 'Vai trabalhar com 16 ou 32 bits (digite 0 para 16, e 1 para 32): '
+    PRECISION_LEN   EQU $-PRECISION
 
-username_len    dd 16
-    
+    MENU_0         db 'ESCOLHA UMA OPÇÃO:'
+    MENU_0_LEN     EQU $-MENU_0
+    MENU_1         db '- 1: SOMA',
+    MENU_1_LEN     EQU $-MENU_1
+    MENU_2         db '- 2: SUBTRACAO',
+    MENU_2_LEN     EQU $-MENU_2
+    MENU_3         db '- 3: MULTIPLICACAO',
+    MENU_3_LEN     EQU $-MENU_3
+    MENU_4         db '- 4: DIVISAO',
+    MENU_4_LEN     EQU $-MENU_4
+    MENU_5         db '- 5: EXPONENCIACAO',
+    MENU_5_LEN     EQU $-MENU_5
+    MENU_6         db '- 6: MOD',
+    MENU_6_LEN     EQU $-MENU_6
+    MENU_7         db '- 7: SAIR',
+    MENU_7_LEN     EQU $-MENU_7
+
 section .bss
-precision resb 1
-username resb 16
+    precision resb 1
+    username resb 16
+    option resb 1
 
 section .text
 global _start
-; extern soma
 
 _start:
-    enter 4,0
-    push WELCOME
-    push WELCOME_LEN
-    call print
-
-    call print_hola
-
+    call welcome_user
     call get_precision
-
-    push precision
-    call printnum
+    call menu
 
 exit:
     mov eax, 1
@@ -44,6 +51,7 @@ exit:
 
 get_precision:
     enter 0,0
+    push eax
 
     push PRECISION
     push PRECISION_LEN
@@ -53,59 +61,131 @@ get_precision:
     push 1
     call read_string
 
-    mov eax, [precision]
-    sub eax, 0x30
+    xor eax, eax
+    mov al, [precision]
+    sub al, 0x30
+    mov [precision], al
 
-    mov [precision], eax ;ARRUMAR
+    ; push precision
+    ; call printnum
 
+    pop eax
     leave
     ret
 
-print_hola:
+welcome_user:
     enter 0,0
+    push eax
+
+    push WELCOME
+    push WELCOME_LEN
+    call print
 
     push username
-    push DWORD [username_len]
+    push 16
     call read_string
 
-    sub al, 1
-    mov byte [username_len], al
+    sub eax, 1 ; remove newline
 
     push HOLA_PREFIX
     push HOLA_PREFIX_LEN
     call print
 
     push username
-    push DWORD [username_len]
+    push eax
     call print
 
     push HOLA_SUFFIX
     push HOLA_SUFFIX_LEN
     call println
 
+    pop eax
+    leave
+    ret
+
+menu:
+    enter 0,0
+
+    call print_menu
+
+    leave
+    ret
+
+print_menu:
+    enter 0,0
+
+    push MENU_0
+    push MENU_0_LEN
+    call println
+
+    push MENU_1
+    push MENU_1_LEN
+    call println
+
+    push MENU_2
+    push MENU_2_LEN
+    call println
+
+    push MENU_3
+    push MENU_3_LEN
+    call println
+
+    push MENU_4
+    push MENU_4_LEN
+    call println
+
+    push MENU_5
+    push MENU_5_LEN
+    call println
+
+    push MENU_6
+    push MENU_6_LEN
+    call println
+
+    push MENU_7
+    push MENU_7_LEN
+    call println
+
     leave
     ret
 
 
+; FUNCOES AUXILIARES
+
 ; printnum (value)
 printnum:
     enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
 
     mov ecx, [EBP+8]
-    mov ecx, [ecx]
-    add ecx, 0x30
+    xor ebx, ebx
+    mov bl, [ecx]
+    add bl, 0x30
+    mov [ecx], ebx
+
         
-    mov eax, 4          ; write syscall
-    mov ebx, 1          ; stdout
-    mov edx, 1          ; 1 byte
+    mov eax, 4
+    mov ebx, 1
+    mov edx, 1
     int 80h
 
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
     leave
     ret 4
 
 ; print (*str, size)
 print:
     enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
 
     mov eax, 4
     mov ebx, 1
@@ -113,12 +193,21 @@ print:
     mov edx, [EBP+8]
     int 80h
 
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+
     leave
     ret 8 
 
 ; println (*str, size)
 println:
     enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
 
     mov eax, 4
     mov ebx, 1
@@ -132,18 +221,32 @@ println:
     mov edx, NWLN_LEN
     int 80h
 
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
+
     leave
     ret 8
 
-;  read_string (*str, size)
+;  read_string (*str, size) -> tamanho da string
 read_string:
     enter 0,0
+    push eax
+    push ebx
+    push ecx
+    push edx
 
     mov eax, 3
     mov ebx, 0
     mov ecx, [EBP+12]
     mov edx, [EBP+8]
     int 80h ; eax -> tamanho da string inputada
+
+    pop edx
+    pop ecx
+    pop ebx
+    add esp, 4 ; pop eax
 
     leave
     ret 8
